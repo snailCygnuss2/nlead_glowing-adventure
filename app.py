5d9c0f18169a4a53bdac9af784c256874f69b4de
Sahil Ahmed <s101@duck.cim>
2023-12-01 11:59:36 +0100

    Formatted and removed code

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-import logging
-logging.basicConfig(filename="record.log", level=logging.DEBUG, format='%(levelname)s:%(name)s: [%(asctime)s] - - %(message)s')
 import os
+import logging
+
+logging.basicConfig(
+    filename="record.log",
+    level=logging.DEBUG,
+    format="%(levelname)s:%(name)s: [%(asctime)s] - - %(message)s",
+)
 from flask import Flask, render_template, request
 import yaml
 import process_file
@@ -13,19 +18,22 @@ app = Flask(__name__)
 with open("process_file.yml", "r") as ymlfile:
     file_params = yaml.safe_load(ymlfile)
 
-app.config['UPLOAD_FOLDER'] = file_params["Dir"]["uploads"]
-app.config['WORKING_DIR'] = file_params["Dir"]["working_dir"]
-app.config['OUTPUT'] = file_params["Dir"]["output"]
-app.config['ALLOWED_FILES'] = {file_params[d]["file_name"] for d in file_params if "file_name" in file_params[d]}
+app.config["UPLOAD_FOLDER"] = file_params["Dir"]["uploads"]
+app.config["WORKING_DIR"] = file_params["Dir"]["working_dir"]
+app.config["OUTPUT"] = file_params["Dir"]["output"]
+app.config["ALLOWED_FILES"] = {
+    file_params[d]["file_name"] for d in file_params if "file_name" in file_params[d]
+}
 
 # Set correct directory
 os.chdir(app.config["WORKING_DIR"])
 
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
+if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+    os.makedirs(app.config["UPLOAD_FOLDER"])
 
-if not os.path.exists(app.config['OUTPUT']):
-    os.makedirs(app.config['OUTPUT'])
+if not os.path.exists(app.config["OUTPUT"]):
+    os.makedirs(app.config["OUTPUT"])
+
 
 # Generate Files
 def process_excel_js(f_name):
@@ -35,40 +43,51 @@ def process_excel_js(f_name):
     # Search for the file params
     file_param = {}
     for file_type in file_params:
-        if "file_name" in file_params[file_type] and file_params[file_type]["file_name"] == f_name:
+        if (
+            "file_name" in file_params[file_type]
+            and file_params[file_type]["file_name"] == f_name
+        ):
             file_param = file_params[file_type]
     if len(file_param) != 0:
-        process_file.generate_out(file_param, app.config["UPLOAD_FOLDER"], app.config["OUTPUT"])
+        process_file.generate_out(
+            file_param, app.config["UPLOAD_FOLDER"], app.config["OUTPUT"]
+        )
         process_file.file_edit_date(app.config["OUTPUT"])
     else:
         app.logger.warning("File Parameters not Found")
 
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_FILES']
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_FILES"]
+    )
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/upload', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload():
-    if 'file' not in request.files:
-        return 'No file part'
+    if "file" not in request.files:
+        return "No file part"
 
-    file = request.files['file']
+    file = request.files["file"]
 
-    if file.filename == '':
-        return 'No selected file'
+    if file.filename == "":
+        return "No selected file"
 
     if file and file.filename in app.config["ALLOWED_FILES"]:
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
         app.logger.info("File %s uploaded succesfully.", file.filename)
-        process_excel_js(file.filename) # Call the conversion function
+        process_excel_js(file.filename)  # Call the conversion function
         return "File Uploaded"
 
     app.logger.warning("File not uploaded. Invalid name/type.")
-    return 'Invalid file type'
+    return "Invalid file type"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- process_file.py
+++ process_file.py
@@ -1,24 +1,11 @@
-import pylightxl as xl
-from datetime import date, datetime
 import os
+from datetime import date, datetime
 import re
-# import filecmp
-# import shutil
-# import subprocess
 import logging
+import pylightxl as xl
 
 # TODO: Combine link with the name
 
-# def copy_files(file_params, cloud_path):
-#     """
-#     Copy modified files from one drive to the current folder for processing
-#     """
-#     fnames = [x["file_name"] for x in file_params]
-#     for fname_dst in fnames:
-#         fname_src = cloud_path + "/" + fname_dst
-#         if not os.path.exists(fname_dst) or not filecmp.cmp(fname_src, fname_dst, shallow=True):
-#             shutil.copyfile(fname_src, fname_dst)
-#             print(f"{fname_dst} – file is updated.")
 
 def check_expiry_date(given_date):
     """
@@ -37,7 +24,7 @@ def check_expiry_date(given_date):
     m = int(matches[0][1])
     d = int(matches[0][2])
     received_date = date(y, m, d)
-    if received_date<=today:
+    if received_date <= today:
         return True
     return False
 
@@ -58,13 +45,13 @@ def generate_out(file_params, upload_path="", save_path=""):
 
     logging.info(f"Processing {fn_paper}")
     db = xl.readxl(fn_paper)
-    
+
     # Find the starting row of the table
     # iterates through column B to find the first cell with data
     check_row = 1
     check_col = 2
     value = db.ws(ws=fn_paper_sheet).index(row=check_row, col=check_col)
-    while value == '':
+    while value == "":
         check_row = check_row + 1
         value = db.ws(ws=fn_paper_sheet).index(row=check_row, col=check_col)
     table_start_row = check_row
@@ -74,7 +61,7 @@ def generate_out(file_params, upload_path="", save_path=""):
     check_row = table_start_row
     check_col = 1
     value = db.ws(ws=fn_paper_sheet).index(row=check_row, col=check_col)
-    while value != '':
+    while value != "":
         check_col = check_col + 1
         value = db.ws(ws=fn_paper_sheet).index(row=check_row, col=check_col)
     table_end_col = check_col - 1
@@ -84,22 +71,25 @@ def generate_out(file_params, upload_path="", save_path=""):
     check_col = 1
 
     value = db.ws(ws=fn_paper_sheet).index(row=check_row, col=check_col)
-    while value != '':
+    while value != "":
         check_row = check_row + 1
         value = db.ws(ws=fn_paper_sheet).index(row=check_row, col=check_col)
     table_end_row = check_row - 1
     table_end_cell = xl.pylightxl.utility_index2address(table_end_row, table_end_col)
 
-
     # Define the table data
-    db.add_nr(name=table_name, ws=fn_paper_sheet, address=f"{table_start_cell}:{table_end_cell}")
+    db.add_nr(
+        name=table_name,
+        ws=fn_paper_sheet,
+        address=f"{table_start_cell}:{table_end_cell}",
+    )
 
     # Find index of Link column to make it hyperlink
     header = db.nr(name=table_name)[0]
     link_idx = header.index("Link")
 
     # find index of date in the file
-    date_col_names = ['Dates', 'Closing date', 'Deadline']
+    date_col_names = ["Dates", "Closing date", "Deadline"]
     for date_col_name in date_col_names:
         if date_col_name in header:
             date_idx = header.index(date_col_name)
@@ -108,20 +98,22 @@ def generate_out(file_params, upload_path="", save_path=""):
     with open(fn_output, "w") as f:
         f.write("var dataSet = [\n")
         for line in db.nr(name=table_name)[1:]:
-            # If the date has expired, do not add it to the list 
+            # If the date has expired, do not add it to the list
             if check_expiry_date(line[date_idx]):
                 continue
-            
+
             f.write("[\n '")
             f.write("', '".join(x.strip().replace("\n", " ") for x in line[:link_idx]))
             f.write("', '")
             f.write(f'<a href="{line[link_idx]}" target="_blank">Link</a>')
             f.write("', '")
-            f.write("', '".join(x.strip().replace("\n", " ") for x in line[link_idx+1:]))
+            f.write(
+                "', '".join(x.strip().replace("\n", " ") for x in line[link_idx + 1 :])
+            )
             f.write("' ],\n")
-        
+
         f.write("];")
-        
+
 
 def file_edit_date(save_path=""):
     """
@@ -135,33 +127,29 @@ def file_edit_date(save_path=""):
         f.write(f"let updated_date = '{date_time}';")
 
 
-# def copy_files_git_folder(file_params):
-#     """
-#     Copy the files to github folder
-#     """
-#     for f in file_params:
-#         file_name = f["out_name"] + ".js"
-#         shutil.copyfile(file_name, f"../nlead_data_edit/data/{file_name}")
-#     shutil.copyfile("updated_date.js", "../nlead_data_edit/data/updated_date.js")
-
-
 if __name__ == "__main__":
-    new_file_params = [{"file_name": "Special issues - call for papers.xlsx",
-                    "sheet_name": "Special issues",
-                    "out_name": "special_issues"},
-                    {"file_name": "Seminars webinars etc.xlsx",
-                    "sheet_name": "Seminars, WS, etc",
-                    "out_name": "seminar_webinar"},
-                    {"file_name": "Research conference calls.xlsx",
-                    "sheet_name": "Research conf",
-                    "out_name": "research_conf"},
-                    {"file_name": "Research calls.xlsx",
-                    "sheet_name": "Research calls",
-                    "out_name": "research_calls"},
-                ]
-    cloud_path = "/Users/sahilah/Library/CloudStorage/OneDrive-Chalmers/Documents - Chalmers.Northern LEAD Shared Folder/Shared/Lists of Research calls, Special issues, Conferences"
-
-    # copy_files(file_params, cloud_path)
+    new_file_params = [
+        {
+            "file_name": "Special issues - call for papers.xlsx",
+            "sheet_name": "Special issues",
+            "out_name": "special_issues",
+        },
+        {
+            "file_name": "Seminars webinars etc.xlsx",
+            "sheet_name": "Seminars, WS, etc",
+            "out_name": "seminar_webinar",
+        },
+        {
+            "file_name": "Research conference calls.xlsx",
+            "sheet_name": "Research conf",
+            "out_name": "research_conf",
+        },
+        {
+            "file_name": "Research calls.xlsx",
+            "sheet_name": "Research calls",
+            "out_name": "research_calls",
+        },
+    ]
 
     for file_param in new_file_params:
         generate_out(file_param)
@@ -169,9 +157,4 @@ if __name__ == "__main__":
     file_edit_date()
     print("JSON files updated")
 
-    # copy_files_git_folder(file_params)
-    # print("Copied files to git folder")
-
-    # Call shell script to push ot github.
-    # subprocess.call(["zsh", "update_git.sh"])
     print("Completed")
